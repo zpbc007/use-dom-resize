99a5053ea6c3ef48a74260beca2653a27191070b
zhaopeng <719197352@qq.com>
2019-03-29 11:37:33 +0800

    chore: change type definition

--- ResizeObserver.d.ts
+++ ResizeObserver.d.ts
@@ -51,18 +51,3 @@ interface Window {
      */
     readonly contentRect: DOMRectReadOnly;
   }
-  
-  interface DOMRectReadOnly {
-    static fromRect(other: DOMRectInit | undefined): DOMRectReadOnly;
-  
-    readonly x: number;
-    readonly y: number;
-    readonly width: number;
-    readonly height: number;
-    readonly top: number;
-    readonly right: number;
-    readonly bottom: number;
-    readonly left: number;
-  
-    toJSON: () => any;
-  }
